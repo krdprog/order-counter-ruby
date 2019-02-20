1c6f7283c02adb30a6a1bf06f7e9229a10ff81b7
krdprog <info@krdprog.ru>
2019-02-20 10:42:10 +0300

    Refactoring and convert tabs to spaces

--- counter.rb
+++ counter.rb
@@ -1,24 +1,24 @@
 order = {}
 
 loop do
-	print 'Введите id продукта: '
-	id_product = gets.strip
+  print 'Введите id продукта: '
+  id_product = gets.strip
 
-	print "Сколько штук вы хотите заказать: "
-	amount_now = gets.strip.to_i
+  print 'Сколько штук вы хотите заказать: '
+  amount_now = gets.strip.to_i
 
-	amount = order[id_product].to_i
-	amount += amount_now
+  amount = order[id_product].to_i
+  amount += amount_now
 
-	order[id_product] = amount
+  order[id_product] = amount
 
-	# calculate total number of items in cart
-	total = 0
+  # calculate total number of items in cart
+  total = 0
 
-	order.each do |key, value|
-		total += value
-	end
+  order.each do |key,value|
+    total += value
+  end
 
-	puts order.inspect
-	puts "Total: #{total}"
+  puts order.inspect
+  puts "Total: #{total}"
 end
